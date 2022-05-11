e0bc49059a9266a9a937cf78a0d08fb59e1d971d
Dnakhutsrishvili <dtnakhutsrishvili@gmail.com>
2022-05-11 19:26:40 +0400

    phoe validation fix and submit form in progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import AddTask from "./Components/AddTask";
-import Quata from "./Components/Quata";
+
 import React from "react";
 
 function App() {
   return (
     <>
-      <Quata></Quata>
       <AddTask></AddTask>
     </>
   );
